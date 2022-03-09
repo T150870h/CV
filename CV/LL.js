11a18db78c3f75f76897a5f3029a0ca5fe0c6307
T150870h <90712503+T150870h@users.noreply.github.com>
2022-03-09 22:14:19 +0700

    CV

--- CV/LL.js
+++ CV/LL.js
@@ -5,6 +5,9 @@ class bgr {
 
         this.$image = document.createElement('img');
         this.$image.src = './image/calendar.png';
+         
+        this.$image_2 = document.createElement('img');
+      
     }
     render() {
         this.$container.appendChild(this.$image);
--- index.js
+++ index.js
@@ -4,15 +4,20 @@ import brg from "./CV/LL.js ";
 class views {
     constructor() {
         this.$container = document.createElement('div');
-        this.$container.setAttribute('class', 'container w-full');
+        this.$container.setAttribute('class', 'flex ');
+        this.$container_1 = document.createElement('div');
         this.$main = new Main();
-        this.$knlv = new Knlv();    
+        this.$knlv = new Knlv(); 
+        this.$container_2 = document.createElement('div');   
         this.$brg = new brg();
     }
     render() {
-        this.$container.appendChild(this.$main.render());
-        this.$container.appendChild(this.$knlv.render());
-        this.$container.appendChild(this.$brg.render());
+        this.$container_1.appendChild(this.$main.render());
+        this.$container_1.appendChild(this.$knlv.render());
+        this.$container_2.appendChild(this.$brg.render());
+        this.$container.appendChild(this.$container_1);
+        this.$container.appendChild(this.$container_2);
+      
         return this.$container;
     }
    
